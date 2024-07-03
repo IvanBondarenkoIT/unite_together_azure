adcefe3de1b87ba45729a1f7767b6fecce84f9b2
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-07-03 14:42:27 +0400

    added: undo changes with static files

--- unite_together_azure/settings.py
+++ unite_together_azure/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 from decouple import config
 
@@ -118,12 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('azure_content/static')),)
-STATIC_URL = '/azure_content/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
